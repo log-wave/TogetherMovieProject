999f8b3b23755fa5319ae8f94f9e2a9116c29114
dvdpi <dvdpi@DESKTOP-GN718D5>
2021-04-24 18:25:56 +0900

    굿즈 페이지에서 admin일때만 상품추가 버튼이 보이게설정

--- src/member/controller/LoginServlet.java
+++ src/member/controller/LoginServlet.java
@@ -1,5 +1,5 @@
 package member.controller;
-//��Ű�� ����
+//占쏙옙키占쏙옙 占쏙옙占쏙옙
 
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -15,75 +15,62 @@ import javax.servlet.http.HttpSession;
 import member.model.service.LoginService;
 import member.model.vo.Member;
 
-
 /**
  * Servlet implementation class LoginServlet
  */
 @WebServlet("/login.do")
 public class LoginServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public LoginServlet() {
-        super();
-        // TODO Auto-generated constructor stub
-    }
 
 	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	    
-		RequestDispatcher dispatcher = request.getRequestDispatcher("contents/loginMain/loginMain.jsp");
-		dispatcher.forward(request, response);
-		
+	public LoginServlet() {
+		super();
+		// TODO Auto-generated constructor stub
 	}
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		String url="contents/loginMain/loginMain.jsp";
-		
-		
-		
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+
+		RequestDispatcher dispatcher = request.getRequestDispatcher("contents/loginMain/loginMain.jsp");
+		dispatcher.forward(request, response);
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		String url = "contents/loginMain/loginMain.jsp";
+
 		String userId = request.getParameter("userId");
 		String userPassword = request.getParameter("userPassword");
-		
+
 		String beforeUrl = request.getParameter("beforeUrl");
-		
-		
-		
+
 		LoginService loginService = new LoginService();
-		int result = loginService.doLogin(userId,userPassword);
-		
-		if(result == 1) {
+		int result = loginService.doLogin(userId, userPassword);
+
+		if (result == 1) {
 			Member member = loginService.getMember(userId);
 			HttpSession session = request.getSession();
 			session.setAttribute("loginUser", member);
-			
+
 			response.sendRedirect(beforeUrl);
-			
-			
-		}else if(result == 0 || result == -1) {
-			request.setAttribute("message", "아이디나 비밀번호가 일치하지 않습니다.");
+
+		} else if (result == 0 || result == -1) {
+			request.setAttribute("message", "�븘�씠�뵒�굹 鍮꾨�踰덊샇媛� �씪移섑븯吏� �븡�뒿�땲�떎.");
 			RequestDispatcher dispatcher = request.getRequestDispatcher(url);
 			dispatcher.forward(request, response);
-		
-		
-		}		
-			
-		
 
-		
 		}
-		
-		
-	
-		
-
-		
+	}
 
 }
