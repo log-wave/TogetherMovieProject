c12c3d0ae567ccaafb3243be293143be7dbef0fe
dvdpi <dvdpi@DESKTOP-GN718D5>
2021-04-24 23:42:40 +0900

    cos.jar를 lib에 추가후 MyFileRenamePolicy 작업

---
+++ src/common/MyFileRenamePolicy.java
@@ -0,0 +1,13 @@
+package common;
+
+import java.io.File;
+
+import com.oreilly.servlet.multipart.FileRenamePolicy;
+
+public class MyFileRenamePolicy implements FileRenamePolicy{
+	@Override
+	public File rename(File arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+}
--- src/goods/controller/InsertGoodsServlet.java
+++ src/goods/controller/InsertGoodsServlet.java
@@ -44,7 +44,6 @@ public class InsertGoodsServlet extends HttpServlet {
 			if(!f.exists()) {
 				f.mkdirs();
 			}
-			
 		}
 		
 	}
